10b08a5e56f58ae9ebb0af8921f4fff0d4bf39f1
turkmuhendisi <1turkmuhendisi@gmail.com>
2025-01-29 01:26:20 +0300

    Vercel analytics implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./components/Navbar";
 import Home from "./pages/home-page/Home.tsx";
 import About from "./pages/About";
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/faq" element={<FAQ />} />
             </Routes>
+            <Analytics /> {/* Vercel Analytics in here */}
         </Router>
     );
 }
