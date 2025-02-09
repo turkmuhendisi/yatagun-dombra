ba7f9bb1598584b10c482855953fbc81ec32929c
turkmuhendisi <1turkmuhendisi@gmail.com>
2025-02-10 02:19:02 +0300

    Bug fixed

---
+++ src/pages/home-page/Home.js
@@ -0,0 +1,7 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import HeroSection from "./section/HeroSection";
+import FeaturedProductsSection from "./section/FeaturedProductsSection";
+import AboutDombraSection from "./section/AboutDombraSection";
+export default function Home() {
+    return (_jsxs("div", { children: [_jsx(HeroSection, {}), _jsx(FeaturedProductsSection, {}), _jsx(AboutDombraSection, {})] }));
+}
