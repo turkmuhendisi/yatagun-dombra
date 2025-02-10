0e12d99fa96651855eb04b760e630eca4783d5bc
turkmuhendisi <1turkmuhendisi@gmail.com>
2025-02-10 04:47:15 +0300

    Product added Some bugs fixed

--- src/pages/home-page/section/FeaturedProductsSection.tsx
+++ src/pages/home-page/section/FeaturedProductsSection.tsx
@@ -3,26 +3,25 @@ import { Product } from "../../../components/types";
 import FeaturedProductCard from "../../../components/FeaturedProductCard";
 
 const FeaturedProducts: React.FC = () => {
-    // Öne çıkan ürünler (useMemo ile optimize edildi)
     const featuredProducts: Product[] = useMemo(() => [
         { id: 1, name: "Bozkır", image: "https://hrow1mwvyrqtjvqe.public.blob.vercel-storage.com/assets/products/dombras/dombra1/d1-1-AJdIFPNKVkAHgrLqMMrDm5Td1eJumI.jpg", price: "8.250₺" },
         { id: 2, name: "Bozkır Nefesi", image: "https://hrow1mwvyrqtjvqe.public.blob.vercel-storage.com/assets/products/dombras/dombra2/d2-1-zW42CGebmBqfrOiLQj9K40ypvWdMiG.jpg", price: "12.750₺" },
         { id: 3, name: "Tuna", image: "https://hrow1mwvyrqtjvqe.public.blob.vercel-storage.com/assets/products/dombras/dombra3/d3-1-okwONo9n0LGskYO9RV3QeIeoxuyykd.jpg", price: "5.750₺" },
         { id: 4, name: "Gök", image: "https://hrow1mwvyrqtjvqe.public.blob.vercel-storage.com/assets/products/dombras/dombra4/d4-1-N4G0FXpZTTuBw6fOHhO70itWBS1ykt.jpg", price: "9.750₺" },
         { id: 5, name: "Altay", image: "https://hrow1mwvyrqtjvqe.public.blob.vercel-storage.com/assets/products/dombras/dombra5/d5-1-jn7lpvi1FL7Uf6Cf5KqxLk9c47TcTm.jpg", price: "5.750₺" },
+        {id: 6, name: "Yılkı", image: "https://hrow1mwvyrqtjvqe.public.blob.vercel-storage.com/assets/products/dombras/dombra6/d6-3-k2jnr6KOApoGoW0NeG3uoesA5tLZBm.webp", price: "5.750₺"},
     ], []);
 
     const containerRef = useRef<HTMLDivElement | null>(null);
     const [isScrollable, setIsScrollable] = useState(false);
 
-    // Kaydırma ihtiyacını kontrol et
     useEffect(() => {
         if (containerRef.current) {
             const containerWidth = containerRef.current.offsetWidth;
             const contentWidth = containerRef.current.scrollWidth;
             setIsScrollable(contentWidth > containerWidth);
         }
-    }, [featuredProducts]); // 🔹 useEffect içinde bağımlılık olarak artık değişmeyecek bir değer var!
+    }, [featuredProducts]);
 
     return (
         <section className="bg-white py-16 h-screen">
--- test.java
+++
@@ -1,7 +0,0 @@
-class JavaTestApp {
-    public static void main(String [] args) {
-        System.out.println("Hello Power MODEEEEE!");
-    }
-}
-
-// This power mode.
