14126425d3065a3262c3ec81a5c80bf89df016da
turkmuhendisi <1turkmuhendisi@gmail.com>
2025-02-10 05:56:14 +0300

    Product detail update -Add to cart button customization -Add to cart button redirect

--- src/pages/product-page/Products.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ProductSection from "./section/ProductSection";
-export default function Products() {
-    return (_jsx("div", { children: _jsx(ProductSection, {}) }));
-}
