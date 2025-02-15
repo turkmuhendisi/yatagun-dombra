d34b98ac7922c461052ddd359ca168dfe614286d
turkmuhendisi <1turkmuhendisi@gmail.com>
2025-02-15 07:00:07 +0300

    Instagram navigation update

--- src/pages/home-page/section/HeroSection.tsx
+++ src/pages/home-page/section/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
 
     return (
         <section className="bg-gray-100 min-h-screen flex flex-col items-center justify-center text-center px-4">
-            <div className="max-w-4xl">
+            <div className="max-w-4xl mt-[-100px]">
                 <h1 className="text-5xl lg:text-7xl font-bold text-gray-900 mb-4 text-left">
                     <span className="text-gray-900">Bozkır ruhunu</span> özgürce hissedin diye...
                 </h1>
@@ -32,11 +32,14 @@ export default function HeroSection() {
                 href="https://www.instagram.com/yatagundombra/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center space-x-2 mt-8 lg:mt-16 font-semibold lg:text-xl transition"
+                className="relative flex items-center space-x-2 mt-8 lg:mt-16 font-semibold lg:text-xl transition rounded-md p-[1px] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
             >
-                <FontAwesomeIcon icon={faInstagram} size="lg" className="text-black" />
-                <span>yatagundombra</span>
+                <div className="flex items-center justify-center w-full h-full bg-white rounded-md px-4 py-2">
+                    <FontAwesomeIcon icon={faInstagram} size="lg" className="text-pink-500" />
+                    <span className="text-black ml-2">yatagundombra</span>
+                </div>
             </a>
+
         </section>
     );
 }
